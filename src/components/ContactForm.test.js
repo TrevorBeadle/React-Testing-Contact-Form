713566579d05377a8bfcc5f5c4530a4001e03cc8
Trevor Beadle <trevor.beadle.1996@gmail.com>
2020-10-01 17:37:12 -0500

    built out test for filling out and submitting the form

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -16,7 +16,6 @@ describe("Testing Contact Form", () => {
     const lastNameInput = screen.getByLabelText(/last name/i);
     const emailInput = screen.getByLabelText(/email/i);
     const messageInput = screen.getByLabelText(/message/i);
-    const submit = screen.getByRole("button", { name: /submit/i });
 
     fireEvent.change(firstNameInput, {
       target: { name: "firstName", value: "Trevor" },
@@ -25,13 +24,69 @@ describe("Testing Contact Form", () => {
     fireEvent.change(lastNameInput, {
       target: { name: "lastName", value: "Trevor" },
     });
+
     fireEvent.change(emailInput, {
       target: { name: "email", value: "Trevor" },
     });
+
     fireEvent.change(messageInput, {
       target: { name: "message", value: "Trevor" },
     });
 
     // Assert
+    const newFirstName = screen.findByText("Trevor");
+    expect(newFirstName).toBeTruthy();
+
+    const newLastName = screen.findByText("Beadle");
+    expect(newLastName).toBeTruthy();
+
+    const newEmail = screen.findByText("trevor@beadle.com");
+    expect(newEmail).toBeTruthy();
+
+    const newMessage = screen.findByText("Hello World");
+    expect(newMessage).toBeTruthy();
+  });
+
+  test("if form can be submitted", () => {
+    // Arrange
+    render(<ContactForm />);
+
+    // Act
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    const lastNameInput = screen.getByLabelText(/last name/i);
+    const emailInput = screen.getByLabelText(/email/i);
+    const messageInput = screen.getByLabelText(/message/i);
+    const submit = screen.getByRole("button", { name: /submit/i });
+
+    fireEvent.change(firstNameInput, {
+      target: { name: "firstName", value: "Trevor" },
+    });
+
+    fireEvent.change(lastNameInput, {
+      target: { name: "lastName", value: "Beadle" },
+    });
+
+    fireEvent.change(emailInput, {
+      target: { name: "email", value: "trevor@beadle.com" },
+    });
+
+    fireEvent.change(messageInput, {
+      target: { name: "message", value: "Hello World" },
+    });
+
+    fireEvent.click(submit);
+
+    // Assert
+    const newFirstName = screen.findByText("Trevor");
+    expect(newFirstName).toBeTruthy();
+
+    const newLastName = screen.findByText("Beadle");
+    expect(newLastName).toBeTruthy();
+
+    const newEmail = screen.findByText("trevor@beadle.com");
+    expect(newEmail).toBeTruthy();
+
+    const newMessage = screen.findByText("Hello World");
+    expect(newMessage).toBeTruthy();
   });
 });
